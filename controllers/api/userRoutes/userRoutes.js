c5b8998346706e6b2b8fb4d77e3a3bb292b8c206
michael seaman <mseaman26@gmail.com>
2025-01-23 15:18:23 -0800

    unswallowing one error

--- controllers/api/userRoutes/userRoutes.js
+++ controllers/api/userRoutes/userRoutes.js
@@ -92,6 +92,7 @@ router.get('/me', auth, async (req, res) => {
 
     res.status(200).json(user);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: err.message });
   }
 });
