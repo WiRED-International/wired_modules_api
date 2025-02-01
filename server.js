4739b10512cf8a3059fb07b6c32494e33e69f178
michael seaman <mseaman26@gmail.com>
2025-01-31 16:50:18 -0800

    server will serve react app from client-build folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const sequelize = require('./config/connection');
 const port = process.env.PORT || 3000;
 const auth = require('./middleware/auth');
+const path = require("path");
 
 const routes = require('./controllers/api/index');
 
@@ -12,6 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.use('/auth', auth);
+
+//serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client-build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client-build", "index.html"));
+});
 
 sequelize.sync({ force: false })
   .then(async() => {
