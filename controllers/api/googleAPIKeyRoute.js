8c683d6cf0f304fb0e53e45dc06c356297156795
Sean Bristol-Lee <seanbristol81@gmail.com>
2026-07-21 03:16:51 -0700

    fix map

--- controllers/api/googleAPIKeyRoute.js
+++ controllers/api/googleAPIKeyRoute.js
@@ -4,7 +4,10 @@ const auth = require('../../middleware/auth');
 
 router.get('/', auth, isSuperAdmin, async (req, res) => {
     try {
-        res.status(200).json({ googleAPIKey: process.env.GOOGLE_API_KEY });
+      
+      res.status(200).json({
+        googleAPIKey: process.env.GOOGLE_API_KEY,
+      });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: err.message });
